0ee2dc482c865425561c2914348f1d7e60322ca4
이은우 <80537533+lew0205@users.noreply.github.com>
2022-10-05 20:14:45 +0900

    MemberService

--- src/main/java/com/crud/crudPractice/member/service/MemberService.java
+++ src/main/java/com/crud/crudPractice/member/service/MemberService.java
@@ -1,4 +1,17 @@
 package com.crud.crudPractice.member.service;
 
+import com.crud.crudPractice.member.domain.Member;
+import com.crud.crudPractice.post.domain.Post;
+
+import java.util.List;
+
 public interface MemberService {
+
+    Long join(Member member);
+
+    List<Member> findMembers();
+
+    Member findMemberById(Long id);
+
+    Member findMemberByName(String name);
 }
--- src/main/java/com/crud/crudPractice/member/service/MemberServiceImpl.java
+++ src/main/java/com/crud/crudPractice/member/service/MemberServiceImpl.java
@@ -1,2 +1,41 @@
-package com.crud.crudPractice.member.service;public class MemberServiceImpl {
+package com.crud.crudPractice.member.service;
+
+import com.crud.crudPractice.member.domain.Member;
+import com.crud.crudPractice.member.repository.MemberRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    @Override
+    public Long join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    @Override
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Override
+    public Member findMemberById(Long id) {
+        return memberRepository.findById(id).orElseThrow(() -> new RuntimeException());
+    }
+
+    @Override
+    public Member findMemberByName(String name) {
+        return memberRepository.findByName(name);
+    }
 }
