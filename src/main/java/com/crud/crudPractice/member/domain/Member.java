f20574178379cf7d391e89f1ecad85ae215dc39a
이은우 <80537533+lew0205@users.noreply.github.com>
2022-10-05 20:14:14 +0900

    Member Domain

--- src/main/java/com/crud/crudPractice/member/domain/Member.java
+++ src/main/java/com/crud/crudPractice/member/domain/Member.java
@@ -1,11 +1,19 @@
 package com.crud.crudPractice.member.domain;
 
+import com.crud.crudPractice.post.domain.Post;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
+import java.util.Map;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
     @Id
