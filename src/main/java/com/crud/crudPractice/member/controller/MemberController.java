d546f2d761f4c1d3d9fd80d151675b429dbcc44a
이은우 <80537533+lew0205@users.noreply.github.com>
2022-10-06 20:25:14 +0900

    Member Controller

--- src/main/java/com/crud/crudPractice/member/controller/MemberController.java
+++ src/main/java/com/crud/crudPractice/member/controller/MemberController.java
@@ -1,18 +1,56 @@
 package com.crud.crudPractice.member.controller;
 
+import com.crud.crudPractice.member.domain.Member;
+import com.crud.crudPractice.member.domain.MemberForm;
 import com.crud.crudPractice.member.repository.MemberRepository;
+import com.crud.crudPractice.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/member")
+import java.util.List;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
     @PostMapping("/join")
-    public void join(){
+    public Member join(MemberForm memberForm) {
+        Member member = new Member();
+
+        member.setName(memberForm.getName());
+        member.setPhoneNum(memberForm.getPhoneNum());
+        member.setAge(memberForm.getAge());
+
+        log.info(memberForm.getName());
+        log.info(member.getName());
+
+        return member;
     }
+
+    @PostMapping("/{memberId}")
+    public Member findMemberId(@PathVariable Long memberId) {
+        Member result = memberService.findMemberById(memberId);
+        return result;
+    }
+
+    @PostMapping("/{memberName}")
+    public Member findMemberName(@PathVariable String memberName) {
+        Member result = memberService.findMemberByName(memberName);
+        return result;
+    }
+
+    @GetMapping
+    public List<Member> memberList() {
+        List<Member> members = memberService.findMembers();
+        return members;
+    }
+
 }
