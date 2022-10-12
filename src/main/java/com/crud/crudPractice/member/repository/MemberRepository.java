cd139541b0380ba8437f267130aa7401d19b9021
이은우 <80537533+lew0205@users.noreply.github.com>
2022-10-12 19:43:12 +0900

    add @Repository

--- src/main/java/com/crud/crudPractice/member/repository/MemberRepository.java
+++ src/main/java/com/crud/crudPractice/member/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package com.crud.crudPractice.member.repository;
 
 import com.crud.crudPractice.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByName(String name);
