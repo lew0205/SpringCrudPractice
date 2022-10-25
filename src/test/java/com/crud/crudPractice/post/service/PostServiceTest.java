ed893cc19b6c098553b6c8b7e49e7e3adf828ae2
이은우 <80537533+lew0205@users.noreply.github.com>
2022-10-25 17:54:38 +0900

    post submit test

--- src/test/java/com/crud/crudPractice/post/service/PostServiceTest.java
+++ src/test/java/com/crud/crudPractice/post/service/PostServiceTest.java
@@ -1,2 +1,43 @@
-package com.crud.crudPractice.post.service;public class PostServiceTest {
+package com.crud.crudPractice.post.service;
+
+import com.crud.crudPractice.member.domain.Member;
+import com.crud.crudPractice.member.domain.MemberDto;
+import com.crud.crudPractice.member.service.MemberService;
+import com.crud.crudPractice.post.domain.Post;
+import com.crud.crudPractice.post.domain.PostDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private MemberService memberService;
+
+    @AfterEach
+    public void afterEach(){
+        postService.deleteAll();
+    }
+
+    @Test
+    public void submit(){
+        //given
+        MemberDto memberDto = new MemberDto("email@gamil.com", "userA", "010-1234-5678", 20L);
+        Member member = memberService.join(memberDto);
+        Long id = member.getId();
+        PostDto postDto = new PostDto("TestArticle","I want to go home",id);
+        //when
+        Post result = postService.submit(postDto);
+        //then
+        assertThat(result.getTitle()).isEqualTo("TestArticle");
+        assertThat(result.getContent()).isEqualTo("I want to go home");
+        assertThat(result.getAuthor().getName()).isEqualTo("userA");
+    }
 }
